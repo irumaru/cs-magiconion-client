989d0755258ca740600bbceffe7d6631762b96fa
irumaru <jibakomaorochi13@outlook.jp>
2024-09-05 07:40:37 +0000

    動いた

--- Program.cs
+++ Program.cs
@@ -4,64 +4,42 @@ using Hello.Shared;
 using Grpc.Core;
 //using Microsoft.AspNetCore.Mvc;
 
-namespace GrpcClient
+string address = "http://localhost:5000";
+
+Console.WriteLine($"Connect server: {address}");
+var channel = GrpcChannel.ForAddress(address);
+
+var client = MagicOnionClient.Create<IAccountService>(channel);
+
+var token = await client.CreateAccountAsync("maru1");
+
+Console.WriteLine(token);
+
+
+// 認証情報をセット
+var credentials = CallCredentials.FromInterceptor((context, metadata) => 
 {
-  class Program
+  if (!string.IsNullOrEmpty(token))
   {
-    private const string address = "http://localhost:5000";
-
-    private static string _token;
-
-    private static GrpcChannel _channel;
-
-    static async Task Main(string[] args)
-    {
-      
-      CreateChannel();
-      CreateAccount();
-    }
-
-    private static void CreateAuthenticatedChannel()
-    {
-      // 認証情報をセット
-      var credentials = CallCredentials.FromInterceptor((context, metadata) => 
-      {
-        if (!string.IsNullOrEmpty(_token))
-        {
-          metadata.Add("Authorization", $"Bearer {_token}");
-        }
-        return Task.CompletedTask;
-      });
-
-      // 認証情報を使用して接続
-      _channel = GrpcChannel.ForAddress(address, new GrpcChannelOptions
-      {
-        Credentials = ChannelCredentials.Create(new SslCredentials(), credentials)
-        //Credentials = ChannelCredentials.Insecure
-      });
-    }
-
-    private static void CreateChannel()
-    {
-      // 接続
-      Console.WriteLine($"Connect server: {address}");
-      var channel = GrpcChannel.ForAddress(address);
-    }
-
-    private static async void CreateAccount()
-    {
-      // 接続
-      var client = MagicOnionClient.Create<IAccountService>(_channel);
-
-      var result = await client.CreateAccountAsync("maru1");
-
-      Console.WriteLine(result);
-      Console.WriteLine(result.ToString());
-
-      _token = result.ToString();
-    }
+    metadata.Add("Authorization", $"Bearer {token}");
   }
-}
+  return Task.CompletedTask;
+});
+
+// 認証情報を使用して接続
+var authChannel = GrpcChannel.ForAddress(address, new GrpcChannelOptions
+{
+  //Credentials = ChannelCredentials.Create(new SslCredentials(), credentials)
+  Credentials = ChannelCredentials.Create(ChannelCredentials.Insecure, credentials),
+  UnsafeUseInsecureChannelCallCredentials = true
+});
+
+var userClient = MagicOnionClient.Create<IHelloService>(authChannel);
+
+var hello = await userClient.SayAsync("World");
+
+Console.WriteLine(hello);
+
 
 /*
 var channel = GrpcChannel.ForAddress("http://localhost:5000");
